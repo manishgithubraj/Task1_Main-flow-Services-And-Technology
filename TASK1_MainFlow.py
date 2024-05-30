66fecfb01db6e3ae315e2387834180b888f372db
Manish Raj <125475647+manishgithubraj@users.noreply.github.com>
2024-05-30 11:39:11 +0530

    Add files via upload

---
+++ TASK1_MainFlow.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # LIST OPERATIONS
+
+# In[5]:
+
+
+# Create a list
+my_list = [1, 2, 3, 4, 5]
+
+# Display original list
+print("Original List:", my_list)
+
+
+# In[2]:
+
+
+# Adding elements
+my_list.append(6)
+
+# Display after adding elements
+print("List after adding elements:", my_list)
+
+
+# In[3]:
+
+
+# Removing elements
+my_list.remove(3)
+
+# Display after removing elements
+print("List after removing elements:", my_list)
+
+
+# In[4]:
+
+
+# Modifying elements
+my_list[2] = 10
+
+# Display after modifying elements
+print("List after modifying elements:", my_list)
+
+
+# # DICTIONARY OPERATIONS
+
+# In[6]:
+
+
+# Create a dictionary
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+
+# Display original dictionary
+print("\nOriginal Dictionary:", my_dict)
+
+
+# In[7]:
+
+
+# Adding elements
+my_dict['d'] = 4
+
+# Display after adding elements
+print("Dictionary after adding elements:", my_dict)
+
+
+# In[8]:
+
+
+# Removing elements
+my_dict.pop('b')
+
+# Display after removing elements
+print("Dictionary after removing elements:", my_dict)
+
+
+# In[9]:
+
+
+# Modifying elements
+my_dict['a'] = 'apple'
+
+# Display after modifying elements
+print("Dictionary after modifying elements:", my_dict)
+
+
+# # SET OPERATIONS
+
+# In[10]:
+
+
+# Create a set
+my_set = {1, 2, 3, 4, 5}
+
+# Display original set
+print("\nOriginal Set:", my_set)
+
+
+# In[11]:
+
+
+# Adding elements
+my_set.add(6)
+
+# Display after adding elements
+print("Set after adding elements:", my_set)
+
+
+# In[12]:
+
+
+# Removing elements
+my_set.remove(4)
+
+# Display after removing elements
+print("Set after removing elements:", my_set)
+
+
+# In[ ]:
+
+
+
+
